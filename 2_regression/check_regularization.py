8403e91d028e4bdd13ca175c8bd610c855f4e5e9
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2024-05-12 20:39:10 +0900

    chore: regr coefficientsと補足説明を追加

--- 2_regression/check_regularization.py
+++ 2_regression/check_regularization.py
@@ -14,7 +14,7 @@ inputs = np.array([[x,x**2,x**3,x**4,x**5,x**6,x**7,x**8] for x in inputs])
 outputs = data[:,1]
 regr = linear_model.LinearRegression()
 regr.fit(inputs,outputs)
-print('Coefficients: \n', regr.coef_)
+print('regr coefficients: \n', regr.coef_)
 
 parameters = {'alpha':[1e-15,1e-10,1e-8,1e-3,1e-2,1,5,10,20,30,35,40,45,50,55,100]}
 ridge = Ridge()
